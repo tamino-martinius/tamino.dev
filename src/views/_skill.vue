62489008de44a5227c0585289489be1289b29b24
tamino-martinius <dev@zaku.eu>
2018-02-10 21:50:42 +0100

    Add partial for skill

---
+++ src/views/_skill.vue
@@ -0,0 +1,36 @@
+<template>
+  <Variable :name="name" type="Skill" :length="16">
+    <span class="expression">{</span>
+    <span class="white-space space"></span>
+    <span class="variable">level</span>
+    <span class="expression">:</span>
+    <span class="white-space space"></span>
+    <Number :value="skill.level.toFixed(1)" />
+    <span class="expression">,</span>
+    <span class="white-space space"></span>
+    <span class="variable">name</span>
+    <span class="expression">:</span>
+    <span class="white-space space"></span>
+    <String :value="skill.name"></String>
+    <span class="white-space space"></span>
+    <span class="expression">}</span>
+  </Variable>
+</template>
+
+<script lang="ts">
+  import Variable from './_variable.vue';
+  import String from './_string.vue';
+  import Number from './_number.vue';
+
+  export default {
+    props: [
+      'name',
+      'skill',
+    ],
+    components: {
+      Variable,
+      String,
+      Number,
+    },
+  };
+</script>
