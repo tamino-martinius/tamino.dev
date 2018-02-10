1dd879e740fdc082123b6badeeac089856ab07c9
tamino-martinius <dev@zaku.eu>
2018-02-10 21:50:03 +0100

    Add partial for comment

---
+++ src/views/_comment.vue
@@ -0,0 +1,25 @@
+<template>
+  <CodeLine>
+    <span class="comment" v-html="commentBody"></span>
+  </CodeLine>
+</template>
+
+<script lang="ts">
+  import CodeLine from './_code_line.vue';
+
+  export default {
+    props: [
+      'comment',
+    ],
+    components: {
+      CodeLine,
+    },
+    computed: {
+      commentBody() {
+        return `// ${this.comment}`
+          .replace(/ /g, '<span class="white-space space"></span>')
+        ;
+      },
+    },
+  };
+</script>
