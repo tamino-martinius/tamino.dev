0b80c9ecbd1866a8f351b461f91ebc222c2ef7fd
tamino-martinius <dev@zaku.eu>
2018-02-10 21:49:43 +0100

    Add partial for string

---
+++ src/views/_string.vue
@@ -0,0 +1,18 @@
+<template>
+  <span class="string" v-html="content"></span>
+</template>
+
+<script lang="ts">
+  export default {
+    props: [
+      'value',
+    ],
+    computed: {
+      content() {
+        return `'${this.value}'`
+          .replace(/ /g, '<span class="white-space space"></span>')
+        ;
+      },
+    },
+  };
+</script>
