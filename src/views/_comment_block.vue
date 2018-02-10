a0d3878f7a67aa4d152d22c41ecd8aebba7dec15
tamino-martinius <dev@zaku.eu>
2018-02-10 21:50:10 +0100

    Add partial for comment block

---
+++ src/views/_comment_block.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="comment-block">
+    <CodeLine>
+      <span class="comment">{{commentHeader}}</span>
+    </CodeLine>
+    <CodeLine>
+      <span class="comment" v-html="commentBody"></span>
+    </CodeLine>
+    <CodeLine>
+      <span class="comment">{{commentFooter}}</span>
+    </CodeLine>
+  </div>
+</template>
+
+<script lang="ts">
+  import CodeLine from './_code_line.vue';
+
+  export default {
+    props: [
+      'comment',
+    ],
+    computed: {
+      commentBody() {
+        return `* ${this.comment} *`
+          .replace(/ /g, '<span class="white-space space"></span>')
+        ;
+      },
+      commentHeader() {
+        return '/' + new Array(this.comment.length + 4).join('*');
+      },
+      commentFooter() {
+        return new Array(this.comment.length + 4).join('*') + '/';
+      },
+    },
+    components: {
+      CodeLine,
+    },
+  };
+</script>
