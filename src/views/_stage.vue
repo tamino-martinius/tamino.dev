0562204f4259f5c2f8b235639f74555800655b70
tamino-martinius <dev@zaku.eu>
2018-02-10 21:51:19 +0100

    Add partial for stage

---
+++ src/views/_stage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="data">
+    <CodeLine>
+      <Tab/>
+      <span class="expression">{</span>
+    </CodeLine>
+    <slot/>
+    <CodeLine>
+      <Tab/><Tab/>
+      <span class="variable">startsAt</span>
+      <span class="expression">:</span>
+      <span class="white-space space"></span>
+      <Date :value="data.startsAt"></Date>
+      <span class="expression">,</span>
+    </CodeLine>
+    <CodeLine>
+      <Tab/><Tab/>
+      <span class="variable">endsAt</span>
+      <span class="expression">:</span>
+      <span class="white-space space"></span>
+      <Date :value="data.endsAt"></Date>
+      <span class="expression">,</span>
+    </CodeLine>
+    <CodeLine>
+      <Tab/><Tab/>
+      <span class="variable">skills</span>
+      <span class="expression">:</span>
+      <span class="white-space space"></span>
+      <span class="expression">[</span>
+    </CodeLine>
+    <CodeLine v-for="(skill, key) in data.skills" :key="key">
+      <Tab/><Tab/><Tab/>
+      <span class="variable">{{key}}</span>
+      <span class="expression">,</span>
+    </CodeLine>
+    <CodeLine>
+      <Tab/><Tab/>
+      <span class="expression">],</span>
+    </CodeLine>
+    <CodeLine>
+      <Tab/><span class="expression">},</span>
+    </CodeLine>
+  </div>
+</template>
+
+<script lang="ts">
+  import CodeLine from './_code_line.vue';
+  import Tab from './_tab.vue';
+  import String from './_string.vue';
+  import Date from './_date.vue';
+
+  export default {
+    props: [
+      'data',
+    ],
+    components: {
+      CodeLine,
+      Tab,
+      String,
+      Date,
+    },
+  };
+</script>
