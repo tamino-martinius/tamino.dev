187889dfc637227aa4a525924ff9e1e516711e94
tamino-martinius <dev@zaku.eu>
2018-02-10 21:49:47 +0100

    Add partial for tab

---
+++ src/views/_tab.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="tab">
+    <span class="white-space space"></span>
+    <span class="white-space space"></span>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+  };
+</script>
