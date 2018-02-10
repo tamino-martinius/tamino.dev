6febb4f750eb69bb67e678aa172e4b0a4c86cdf9
tamino-martinius <dev@zaku.eu>
2018-02-10 21:51:08 +0100

    Add partial for cv

---
+++ src/views/_curriculum_vitae.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="curriculum-vitae">
+    <CodeLine>
+      <span class="storage-type">const</span>
+      <span class="white-space space"></span>
+      <span class="variable">curriculumVitae</span>
+      <span class="keyword">:</span>
+      <span class="white-space space"></span>
+      <span class="expression">(</span>
+      <span class="type class">Job</span>
+      <span class="white-space space"></span>
+      <span class="keyword">|</span>
+      <span class="white-space space"></span>
+      <span class="type class">Education</span>
+      <span class="expression">)[]</span>
+      <span class="white-space space"></span>
+      <span class="expression">=</span>
+      <span class="white-space space"></span>
+      <span class="expression">[</span>
+    </CodeLine>
+    <template v-for="(entry, index) in data">
+      <CodeLine v-if="index !== 0" :key="index"/>
+      <component
+        :is="entry.company ? 'Job' : 'Education'"
+        :key="index"
+        :data="entry"
+      />
+    </template>
+    <CodeLine>
+      <span class="expression">];</span>
+    </CodeLine>
+  </div>
+</template>
+
+<script lang="ts">
+  import CodeLine from './_code_line.vue';
+  import Job from './_job.vue';
+  import Education from './_education.vue';
+
+  export default {
+    props: [
+      'data',
+    ],
+    components: {
+      CodeLine,
+      Job,
+      Education,
+    },
+  };
+</script>
