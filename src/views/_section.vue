eb68267c0700caebf6b2f95b63fc2896faed4575
tamino-martinius <dev@zaku.eu>
2018-02-10 21:51:14 +0100

    Add partial for section

---
+++ src/views/_section.vue
@@ -0,0 +1,25 @@
+<template>
+  <CommentBlock :comment="title"></CommentBlock>
+</template>
+
+<script lang="ts">
+  import {
+    Section,
+  } from '../data_types';
+
+  import CommentBlock from './_comment_block.vue';
+
+  export default {
+    props: [
+      'section',
+    ],
+    computed: {
+      title() {
+        return Section[this.section];
+      },
+    },
+    components: {
+      CommentBlock,
+    },
+  };
+</script>
