7b65a1771222157dc57529847b5aa7ddfd8b0a01
tamino-martinius <dev@zaku.eu>
2018-02-10 21:49:35 +0100

    Add partial for number

---
+++ src/views/_number.vue
@@ -0,0 +1,11 @@
+<template>
+  <span class="number" :style="{ '--value': value }">{{value}}</span>
+</template>
+
+<script lang="ts">
+  export default {
+    props: [
+      'value',
+    ],
+  };
+</script>
