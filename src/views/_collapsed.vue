2a1671cf7c74c00c2c91e2098d445d84b1f4f91b
tamino-martinius <dev@zaku.eu>
2018-02-12 07:14:33 +0100

    Add partial for collapsed

---
+++ src/views/_collapsed.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="collapsed">‹…›</span>
+</template>
+
+<script lang="ts">
+  import CodeLine from './_code_line.vue';
+
+  export default {
+    components: {
+      CodeLine,
+    },
+  };
+</script>
