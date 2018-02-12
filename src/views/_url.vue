9d6c13049e1d3725271ad25d3e28978dc44aca3f
tamino-martinius <dev@zaku.eu>
2018-02-12 07:19:15 +0100

    Add partial for urls

---
+++ src/views/_url.vue
@@ -0,0 +1,13 @@
+<template>
+  <a class="string" :href="value" target="_blank">'{{value}}'</a>
+</template>
+
+<script lang="ts">
+  import { formatCode } from '../util';
+
+  export default {
+    props: [
+      'value',
+    ],
+  };
+</script>
