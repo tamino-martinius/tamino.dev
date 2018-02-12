16eefbf2614520af73f61b90e6012312e22cc5ea
tamino-martinius <dev@zaku.eu>
2018-02-12 07:14:59 +0100

    Refator comment partial - add slot for additional data

--- src/views/_comment.vue
+++ src/views/_comment.vue
@@ -1,24 +1,29 @@
 <template>
   <CodeLine>
+    <Tab v-for="i in indentation" :key="i"/>
     <span class="comment" v-html="commentBody"></span>
+    <slot/>
   </CodeLine>
 </template>
 
 <script lang="ts">
+  import { formatCode } from '../util';
+
   import CodeLine from './_code_line.vue';
+  import Tab from './_tab.vue';
 
   export default {
     props: [
+      'indentation',
       'comment',
     ],
     components: {
       CodeLine,
+      Tab,
     },
     computed: {
       commentBody() {
-        return `// ${this.comment}`
-          .replace(/ /g, '<span class="white-space space"></span>')
-        ;
+        return formatCode(`// ${this.comment}`);
       },
     },
   };
