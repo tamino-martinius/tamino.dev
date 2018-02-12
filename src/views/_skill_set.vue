85ff271f63bb29219440a212f13c015ab7279a51
tamino-martinius <dev@zaku.eu>
2018-02-12 07:18:39 +0100

    Skillset is now collapsible

--- src/views/_skill_set.vue
+++ src/views/_skill_set.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="skillset">
-    <Comment :comment="title"></Comment>
-    <template v-if="Array.isArray(skillset.data)">
-      <SkillSet
-        v-for="subset in skillset.data"
-        :title="title + ' / ' + subset.title"
-        :skillset="subset"
-        :key="subset.title"
-      />
+  <div class="skillset" @click.stop="toggle" :class="{ collapsed: collapsed }">
+    <Comment :comment="title"><Collapsed v-if="collapsed"/></Comment>
+    <template v-if="!collapsed">
+      <template v-if="Array.isArray(skillset.data)">
+        <SkillSet
+          v-for="subset in skillset.data"
+          :title="title + ' / ' + subset.title"
+          :skillset="subset"
+          :key="subset.title"
+        />
+      </template>
+      <template v-else v-for="(skill, name) in skillset.data">
+        <Skill
+          :name="name"
+          :skill="skill"
+          :key="name"
+        />
+      </template>
+      <CodeLine/>
     </template>
-    <template v-else v-for="(skill, name) in skillset.data">
-      <Skill
-        :name="name"
-        :skill="skill"
-        :key="name"
-      />
-    </template>
-    <CodeLine/>
   </div>
 </template>
 
@@ -24,17 +26,29 @@
   import CodeLine from './_code_line.vue';
   import Skill from './_skill.vue';
   import Comment from './_comment.vue';
+  import Collapsed from './_collapsed.vue';
 
   export default {
     name: 'SkillSet',
+    data() {
+      return {
+        collapsed: true,
+      };
+    },
     props: [
       'title',
       'skillset',
     ],
+    methods: {
+      toggle() {
+        this.collapsed = !this.collapsed;
+      },
+    },
     components: {
       CodeLine,
       Skill,
       Comment,
+      Collapsed,
     },
   };
 </script>
