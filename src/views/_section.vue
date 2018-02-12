099ced4473f82c3e8c3335086e9c671cdbeee582
tamino-martinius <dev@zaku.eu>
2018-02-12 07:17:45 +0100

    Add hover to section which enables to navigate to other sections

--- src/views/_section.vue
+++ src/views/_section.vue
@@ -1,25 +1,99 @@
 <template>
-  <CommentBlock :comment="title"></CommentBlock>
+  <div class="section" :data-value="title">
+    <CodeLine>
+      <span class="comment">/</span>
+      <template v-for="section in sections">
+        <span
+          class="comment"
+          :class="{
+            collapsible: title !== section,
+            active: title === section,
+          }"
+          :key="`comment-${section}`"
+        >{{stars(section)}}</span>
+      </template>
+      <span class="comment">**</span>
+    </CodeLine>
+    <CodeLine>
+      <span class="comment">*</span>
+      <template v-for="section in sections">
+        <span
+          class="white-space space"
+          :class="{
+            collapsible: title !== section,
+          }"
+          :key="`space-${section}`"
+        />
+        <span
+          class="comment selectable"
+          :class="{
+            collapsible: title !== section,
+            active: title === section,
+          }"
+          :key="`comment-${section}`"
+          @click="scrollToSection(section)"
+        >{{section}}</span>
+      </template>
+      <span class="white-space space" :key="`space-${section}`"></span>
+      <span class="comment">*</span>
+    </CodeLine>
+    <CodeLine>
+      <span class="comment">*</span>
+      <template v-for="section in sections">
+        <span
+          class="comment"
+          :class="{
+            collapsible: title !== section,
+            active: title === section,
+          }"
+          :key="`comment-${section}`"
+        >{{stars(section)}}</span>
+      </template>
+      <span class="comment">*/</span>
+    </CodeLine>
+  </div>
 </template>
 
 <script lang="ts">
   import {
     Section,
   } from '../data_types';
+  import { animate, Easing } from '../util';
 
-  import CommentBlock from './_comment_block.vue';
+  import CodeLine from './_code_line.vue';
 
   export default {
     props: [
       'section',
     ],
+    methods: {
+      stars(str: string) {
+        return new Array(str.length + 2).join('*');
+      },
+      scrollToSection(name: string) {
+        if (name !== this.title) {
+          const target = document.querySelector(`.section[data-value=${name}]`);
+          animate({
+            target: document.querySelector('html'),
+            key: 'scrollTop',
+            value: (<HTMLElement>target).offsetTop,
+            duration: 300,
+            ease: Easing.EaseInOut,
+          });
+        }
+      },
+    },
     computed: {
+      sections() {
+        const keys = Object.keys(Section);
+        return keys.slice(keys.length / 2);
+      },
       title() {
         return Section[this.section];
       },
     },
     components: {
-      CommentBlock,
+      CodeLine,
     },
   };
 </script>
