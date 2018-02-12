997b478e43c1228a800730f552d4ee6e73b7ee30
tamino-martinius <dev@zaku.eu>
2018-02-12 07:14:11 +0100

    Add center as util function

--- src/util.ts
+++ src/util.ts
@@ -2,3 +2,14 @@ export function formatCode(str: string) {
   return str.replace(/ /g, '<span class="white-space space"></span>');
 };
 
+export function center(str: string, len: number) {
+  if (str.length >= len) {
+    return str;
+  } else {
+    const padTotal = len - str.length;
+    const padStart = Math.floor(padTotal / 2) + 1;
+    const padEnd = Math.ceil(padTotal / 2) + 1;
+    return new Array(padStart).join(' ') + str + new Array(padEnd).join(' ');
+  }
+};
+
