5a7d5c6d91b80f9ffa928fb4c6f2f734689c8867
tamino-martinius <dev@zaku.eu>
2018-02-12 07:16:40 +0100

    Add partial for legal

---
+++ src/views/_legal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="legal" @click.stop="toggle" :class="{ collapsed: collapsed }">
+    <Comment
+      v-if="collapsed"
+      :comment="`(c) ${year} Tamino Martinius`"
+    >
+      <Collapsed/>
+    </Comment>
+    <CommentBlock
+      v-else
+      :comment="`(c) ${year}\n\nTamino Martinius\nLehdenstr.  21\n06847 Dessau\n\nhi@zaku.eu`"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+  import Comment from './_comment.vue';
+  import CommentBlock from './_comment_block.vue';
+  import Collapsed from './_collapsed.vue';
+
+  export default {
+    data() {
+      return {
+        collapsed: true,
+      };
+    },
+    methods: {
+      toggle() {
+        this.collapsed = !this.collapsed;
+      },
+    },
+    computed: {
+      year() {
+        return new Date().getFullYear();
+      },
+    },
+    components: {
+      Comment,
+      CommentBlock,
+      Collapsed,
+    },
+  };
+</script>
