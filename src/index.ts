9dac98fba8eb91f72878f36a4be7eb6be38c831b
tamino-martinius <dev@zaku.eu>
2018-02-14 22:10:59 +0100

    Change init

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ const View = require('./views/index.vue').default;
 require('./style.styl');
 import Vue from 'vue';
 
-const root = document.createElement('div');
-root.id = 'view';
-document.body.appendChild(root);
-
 new Vue({
   el: '#view',
   render: display => display(View),
--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,5 @@
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 OfflinePluginRuntime.install();
-window.setTimeout(() => {
-  document.querySelector('.loader').classList.add('hidden');
-}, 1000);
 
 OfflinePluginRuntime.install({
   onUpdateReady: function () {
