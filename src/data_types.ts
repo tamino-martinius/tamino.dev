33c45cb936680917be60614dfc8c12141bdbb5ad
tamino-martinius <dev@zaku.eu>
2018-02-16 07:29:20 +0100

    Add langauge and languageHelper

--- src/data_types.ts
+++ src/data_types.ts
@@ -9,6 +9,18 @@ export enum Section {
   Misc,
 };
 
+export enum Language {
+  ruby,
+  typescript,
+  javascript,
+};
+
+export const languages: Language[] = [
+  Language.ruby,
+  Language.typescript,
+  Language.javascript,
+];
+
 export interface Stage {
   description?: string,
   startsAt: Date,
@@ -41,4 +53,48 @@ export interface Skill {
 export interface SkillSet {
   title: string,
   data: SkillSet[] | { [key: string]: Skill },
+};
+
+export class State {
+  currentLanguage: Language;
+  currentLanguageHelper: LanguageHelper;
+
+  private getLanguageClass(lang: Language): string {
+    return `lang-${Language[lang]}`;
+  }
+
+  get language(): Language {
+    return this.currentLanguage;
+  }
+
+  set language(lang: Language) {
+    this.currentLanguage = lang;
+    this.currentLanguageHelper = new LanguageHelper(lang);
+    for (const language of languages) {
+      document.body.classList.remove(this.getLanguageClass(language));
+    }
+    document.body.classList.add(this.getLanguageClass(lang));
+  }
+};
+
+
+export class LanguageHelper {
+  constructor(private language: Language) {
+  }
+
+  get commentChar() {
+    return this.language === Language.ruby ? '#' : '*';
+  }
+
+  get commentEnd() {
+    return this.language === Language.ruby ? '#' : '/';
+  }
+
+  get comment() {
+    return this.language === Language.ruby ? '#' : '//';
+  }
+
+  get undefined() {
+    return this.language === Language.ruby ? 'nil' : 'undefined';
+  }
 };
