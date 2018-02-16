778e016541cae5fba78937d5cba3bcd654bc9ffa
tamino-martinius <dev@zaku.eu>
2018-02-16 07:30:42 +0100

    Init language and change on keypress

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,43 @@ require('./init');
 const View = require('./views/index.vue').default;
 require('./style.styl');
 import Vue from 'vue';
-
-new Vue({
-  el: '#view',
-  render: display => display(View),
-});
+import { state } from './data';
+import { Language, languages } from './data_types';
 
 let theme = 0;
 const themeCount = 3;
 
+if (state.language === undefined) {
+  state.language = languages[Math.floor(Math.random() * languages.length)];
+}
+
 document.addEventListener('keydown', function (e) {
-  if (e.which === 32) {
-    e.preventDefault();
-    theme += 1;
-    for (var i = 0; i < themeCount; i++) {
-      document.body.classList.remove(`theme-${i}`);
+  switch (e.which) {
+    case 32: {
+      e.preventDefault();
+      theme += 1;
+      for (var i = 0; i < themeCount; i++) {
+        document.body.classList.remove(`theme-${i}`);
+      }
+      document.body.classList.add(`theme-${theme % themeCount}`);
+      break;
     }
-    document.body.classList.add(`theme-${theme % themeCount}`);
+    case 82: {
+      state.language = Language.ruby;
+      break;
+    }
+    case 74: {
+      state.language = Language.javascript;
+      break;
+    }
+    case 84: {
+      state.language = Language.typescript;
+      break;
+    }
   }
+});
+
+new Vue({
+  el: '#view',
+  render: display => display(View),
 });
