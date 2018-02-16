00b411e04af4236e9a72dd0128af68615a379d26
tamino-martinius <dev@zaku.eu>
2018-02-16 18:52:47 +0100

    Move profile to partial

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,18 +1,7 @@
 <template>
   <div id="view" class="hidden">
-    <Section :section="Section.Header"></Section>
     <CodeLine/>
-    <Variable name="myName">
-      <String :value="data.myName"></String>
-    </Variable>
-    <Variable name="profession">
-      <String :value="data.profession"></String>
-    </Variable>
-    <Variable name="email">
-      <String :value="email"></String>
-    </Variable>
-    <CodeLine/>
-    <CodeLine/>
+    <Profile :data="data"/>
     <Section :section="Section.Skills"></Section>
     <CodeLine/>
     <template v-for="skillset in data.skills">
@@ -59,25 +48,18 @@
 
 <script lang="ts">
   import * as data from '../data';
-  import {
-    Section as SectionType,
-  } from '../data_types';
 
-  import Job from './_job.vue';
   import SkillSet from './_skill_set.vue';
   import Section from './_section.vue';
   import CodeLine from './_code_line.vue';
-  import Variable from './_variable.vue';
-  import String from './_string.vue';
   import CurriculumVitae from './_curriculum_vitae.vue';
   import ShowCaseSection from './_show_case_section.vue';
-  import Comment from './_comment.vue';
   import Legal from './_legal.vue';
+  import Profile from "./_profile.vue";
 
   export default {
     data() {
       return {
-        Section: SectionType,
         data,
       };
     },
@@ -85,22 +67,15 @@
       year() {
         return new Date().getFullYear();
       },
-      email() {
-        const emails = this.data.emails;
-        return emails[Math.floor(Math.random() * emails.length)];
-      },
     },
     components: {
-      Job,
       SkillSet,
       Section,
       CodeLine,
-      Variable,
-      String,
       CurriculumVitae,
       ShowCaseSection,
-      Comment,
       Legal,
+      Profile,
     },
   };
 </script>
