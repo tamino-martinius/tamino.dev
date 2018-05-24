acb0f84390eb2b1d077f6950d32c412dcd8c4519
Tamino Martinius <dev@zaku.eu>
2018-05-24 07:45:01 +0200

    Add component for cookie info

---
+++ src/views/_cookie_info.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cookie-info" @click.stop="hide" :class="{ hidden: hidden }">
+    <CommentBlock
+      comment="Please Note: This page uses cookies to count visitors and to save if this message is closed"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+  import { track } from '../util';
+
+  import Comment from './_comment.vue';
+  import CommentBlock from './_comment_block.vue';
+  import Collapsed from './_collapsed.vue';
+
+  const cookieStr = document.cookie;
+  let cookie = { cookieInfo: false };
+  if (cookieStr) {
+    cookieStr.split(';').forEach((cookieProperty) => {
+      if (cookieProperty.indexOf('=') >= 0) {
+        const [key, value] = cookieProperty.split('=');
+        if (key.trim() === 'json') cookie = JSON.parse(value);
+      }
+    });
+  }
+
+  export default {
+    data() {
+      return {
+        hidden: cookie.cookieInfo,
+      };
+    },
+    methods: {
+      hide() {
+        cookie.cookieInfo = true;
+        document.cookie = 'json=' + JSON.stringify(cookie);
+        this.hidden = true;
+      },
+    },
+    components: {
+      CommentBlock,
+    },
+  };
+</script>
