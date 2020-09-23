c81ec410fb487305efc86ef1cd8d2b0675f3801d
Tamino Martinius <dev@zaku.eu>
2020-09-23 19:39:12 +0200

    Update webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
-const resolve = require("path").resolve;
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const OfflinePlugin = require("offline-plugin");
-const UglifyJsWebpackPlugin = require("uglifyjs-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const resolve = require('path').resolve;
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OfflinePlugin = require('offline-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 /// Rules
 
@@ -13,10 +12,10 @@ const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const stylusRule = {
   test: /\.styl$/,
   use: [
-    "style-loader",
-    "css-loader",
+    'style-loader',
+    'css-loader',
     {
-      loader: "stylus-loader",
+      loader: 'stylus-loader',
       options: {
         use: [
           // Stylus plugins
@@ -29,16 +28,16 @@ const stylusRule = {
 // CSS
 const cssRule = {
   test: /\.css$/,
-  use: ["style-loader", "css-loader"],
+  use: ['style-loader', 'css-loader'],
 };
 
 // Vue
 const vueRule = {
   test: /\.vue$/,
-  loader: "vue-loader",
+  loader: 'vue-loader',
   options: {
     cssModules: {
-      localIdentName: "[path][name]---[local]---[hash:base64:5]",
+      localIdentName: '[path][name]---[local]---[hash:base64:5]',
       camelCase: true,
     },
   },
@@ -47,14 +46,14 @@ const vueRule = {
 // YamlLoader
 const yamlRule = {
   test: /\.ya?ml$/,
-  loader: "yml-loader",
+  loader: 'yml-loader',
   exclude: /node_modules/,
 };
 
 // TypeScript
 const tsRule = {
   test: /\.tsx?$/,
-  loader: "ts-loader",
+  loader: 'ts-loader',
   exclude: /node_modules/,
   options: {
     appendTsSuffixTo: [/\.vue$/],
@@ -64,51 +63,24 @@ const tsRule = {
 // Images
 const imageRule = {
   test: /\.(png|svg|jpg|gif)$/,
-  loader: "file-loader",
+  loader: 'file-loader',
   options: {
-    name: "[name].[ext]?[hash]",
+    name: '[name].[ext]?[hash]',
   },
 };
 
 // XML
 const xmlRule = {
   test: /\.xml$/,
-  use: ["xml-loader"],
-};
-
-/// Plugin Options
-
-// CleanWebpackPlugin
-const cleanPaths = ["dist/*"];
-const cleanOptions = {
-  root: resolve("."),
-  exlude: [".keep"],
-};
-
-// Runtime Chunk
-const runtimeChunkOptions = {
-  name: "runtime",
-};
-
-// Vendor Chunk
-const vendorChunkOptions = {
-  name: "vendor",
-};
-
-// Minify
-var minifyOptions = {
-  sourceMap: false,
-  uglifyOptions: {
-    ecma: 6,
-  },
+  use: ['xml-loader'],
 };
 
 /// Export
-const title = "~/tamino.dev/main.ts";
-const path = "/";
+const title = '~/tamino.dev/main.ts';
+const path = '/';
 
 const transform = (content, filePath) => {
-  if (filePath.endsWith(".json") || filePath.endsWith(".xml")) {
+  if (filePath.endsWith('.json') || filePath.endsWith('.xml')) {
     return content
       .toString()
       .replace(/#\{title\}/g, title)
@@ -118,12 +90,13 @@ const transform = (content, filePath) => {
   }
 };
 const config = {
-  entry: "./index.ts",
+  mode: 'production',
+  entry: './index.ts',
   output: {
-    filename: "[name].[hash].js",
-    path: resolve("dist"),
+    filename: '[name].[hash].js',
+    path: resolve('dist'),
   },
-  context: resolve("src"),
+  context: resolve('src'),
   performance: {
     hints: false,
   },
@@ -134,24 +107,24 @@ const config = {
     rules: [yamlRule, stylusRule, cssRule, vueRule, imageRule, xmlRule, tsRule],
   },
   resolve: {
-    extensions: [".ts", ".js", ".vue", ".json"],
+    extensions: ['.ts', '.js', '.vue', '.json'],
     alias: {
-      vue$: "vue/dist/vue.esm.js",
+      vue$: 'vue/dist/vue.esm.js',
     },
   },
   plugins: [
     new VueLoaderPlugin(),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
-      patterns: [{ transform, from: "public" }],
+      patterns: [{ transform, from: 'public' }],
     }),
     new webpack.HashedModuleIdsPlugin(),
     new HtmlWebpackPlugin({
       path,
       title,
-      template: "index.html",
+      template: 'index.html',
       minify:
-        process.env.NODE_ENV === "production"
+        process.env.NODE_ENV === 'production'
           ? {
               minifyJS: true,
               minifyCSS: true,
@@ -163,42 +136,41 @@ const config = {
   ],
 };
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   config.plugins.push(
     new webpack.DefinePlugin({
-      "process.env": {
+      'process.env': {
         NODE_ENV: '"production"',
       },
-    })
+    }),
   );
-  config.plugins.push(new UglifyJsWebpackPlugin(minifyOptions));
   config.plugins.push(
     new OfflinePlugin({
-      publicPath: "/",
-      externals: ["/"],
-      updateStrategy: "changed",
+      publicPath: '/',
+      externals: ['/'],
+      updateStrategy: 'changed',
       autoUpdate: 1000 * 60 * 2,
       caches: {
-        main: ["index.html", "main.*.js"],
+        main: ['index.html', 'main.*.js'],
       },
       ServiceWorker: {
         events: true,
-        navigateFallbackURL: "/",
+        navigateFallbackURL: '/',
       },
       AppCache: {
         events: true,
       },
-    })
+    }),
   );
 } else {
-  config.devtool = "#eval-source-map";
+  config.devtool = '#eval-source-map';
   config.devServer = {
     port: 3000,
     hot: true,
-    host: "localhost",
+    host: 'localhost',
     historyApiFallback: true,
     noInfo: false,
-    contentBase: "./dist",
+    contentBase: './dist',
   };
 }
 
